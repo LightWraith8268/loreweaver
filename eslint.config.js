915834f1332ef4034d280dddfa85b212717b9896
Patrick Antrobus <pantrobus@aims.edu>
2025-09-29 20:49:03 -0600

    chore: exclude generated artifacts from eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,14 @@ const expoConfig = require('eslint-config-expo/flat');
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ["dist/*"],
+    ignores: [
+      'dist/**',
+      'dist-fixed/**',
+      'dist-new/**',
+      'web-build/**',
+      'web-build-old/**',
+      'web-dist/**',
+      'win/**',
+    ],
   }
 ]);
